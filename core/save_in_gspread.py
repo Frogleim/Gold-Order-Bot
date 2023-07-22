f042239322254a496b5486c12064df4979ceac9b
Frogleim <barseghyangor8@gmail.com>
2023-07-22 18:37:29 +0400

    docker files added

--- core/save_in_gspread.py
+++ core/save_in_gspread.py
@@ -1,9 +1,7 @@
 import gspread
-from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import pandas as pd
 
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
           'https://www.googleapis.com/auth/drive']
